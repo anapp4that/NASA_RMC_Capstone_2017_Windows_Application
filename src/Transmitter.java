f79c42e7b4976262a5b49cd164487e9e35ec8bea
incoruptable <zachbrandt51@yahoo.com>
2017-04-05 14:27:13 -0500

    Transmitter changes for String and other minor things.

--- src/Transmitter.java
+++ src/Transmitter.java
@@ -3,6 +3,10 @@ import java.io.IOException;
 import java.io.PrintWriter;
 import java.net.Socket;
 import java.net.SocketTimeoutException;
+import java.util.BitSet;
+import java.util.stream.IntStream;
+
+import static javax.swing.UIManager.get;
 
 /**
  * Created by Incoruptable on 2/5/2017.
@@ -10,21 +14,21 @@ import java.net.SocketTimeoutException;
 public class Transmitter extends Thread {
     private Socket compSocket;
     private Listener listener;
+    private PrintWriter printWriter;
 
-    public Transmitter(int port) throws IOException {
+    public Transmitter() throws IOException {
         compSocket = new Socket(Constants.SERVER_IP_ADDRESS, Constants.SERVER_PORT);
         compSocket.setSoTimeout(10000);
         DataOutputStream dout = new DataOutputStream(compSocket.getOutputStream());
-        PrintWriter printWriter = new PrintWriter(dout, true);
+        printWriter = new PrintWriter(dout, true);
         printWriter.println("comp");
         listener = new Listener();
         listener.start();
     }
 
     public static void main(String[] args) {
-        int port = Integer.parseInt(args[0]);
         try {
-            Thread transmitter = new Transmitter(port);
+            Thread transmitter = new Transmitter();
             transmitter.start();
         } catch (IOException e) {
             e.printStackTrace();
@@ -32,46 +36,36 @@ public class Transmitter extends Thread {
     }
 
     public void run() {
+        String previousSend = "";
+        String currentSend;
         while (true) {
-            try {
-                System.out.println("Just connected to " + compSocket.getRemoteSocketAddress());
-
-                byte[] previousSend = null;
-                byte[] currentSend;
-                while (true) {
-                    DataOutputStream out = new DataOutputStream(compSocket.getOutputStream());
-                    currentSend = listener.getTranslator().bitArray.toByteArray();
-                    if (currentSend != null && !currentSend.equals(previousSend)) {
-                        if (currentSend.length < 3) {
-                            out.writeInt(currentSend.length);
-                        } else {
-                            out.flush();
-                            try {
-                                Thread.sleep(200);
-                            } catch (InterruptedException ex) {
-                                ex.printStackTrace();
-                            }
-                            continue;
-                        }
-                        out.write(currentSend);
-                        previousSend = currentSend;
-                        out.flush();
-                        try {
-                            Thread.sleep(200);
-                        } catch (InterruptedException e) {
-                            e.printStackTrace();
-                        }
+            BitSet bitArray = listener.getTranslator().bitArray;
+            currentSend = bitSetToString(bitArray);
+            if (currentSend != null && !currentSend.equals(previousSend)) {
+                if (currentSend.length() == 13) {
+                    printWriter.write(currentSend);
+                    previousSend = currentSend;
+                } else {
+                    try {
+                        Thread.sleep(200);
+                    } catch (InterruptedException ex) {
+                        ex.printStackTrace();
                     }
+                    continue;
                 }
-
-            } catch (SocketTimeoutException s) {
-                System.out.print("Socket timed out!");
-            } catch (IOException e) {
-                e.printStackTrace();
-                break;
+                try {
+                    Thread.sleep(200);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
             }
-
         }
 
+    }
+
+    private String bitSetToString(BitSet bitSet){
+        final StringBuilder buffer = new StringBuilder(13);
+        IntStream.range(0, 13).mapToObj(i -> get(i) ? '1' : '0').forEach(buffer::append);
+        return buffer.toString();
     }
 }
